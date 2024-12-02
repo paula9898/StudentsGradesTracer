a9e683e7b1ab2c0f22e8f446cbac941856688cac
paula9898 <otrebskapaulina@gmail.com>
2024-12-02 18:50:40 +0100

    create first test SubjectMangerTest

--- src/test/java/pl/paulina/SubjectMangerTest.java
+++ src/test/java/pl/paulina/SubjectMangerTest.java
@@ -2,21 +2,33 @@ package pl.paulina;
 
 
 import org.junit.jupiter.api.Test;
+import pl.paulina.Subject.Subject;
+import pl.paulina.Subject.SubjectManager;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SubjectMangerTest {
+
+    private final SubjectManager subjectManager = new SubjectManager();
 
     @Test
     void addedSubjectShouldBeCreatedTest() {
 
         //given
-
+        String name = "Clients in Netzwerke einbinden";
+        String teacherName = "Rainer";
+        String lernFeld = "LF3";
+        //Subject subject = new Subject(null, name, teacherName, lernFeld);
 
         //when
+        Subject subject = subjectManager.addSubject(name,teacherName, lernFeld);
 
         //then
-
-
-
+        //assertEquals()
+        ArrayList<String> subjects = subjectManager.showAllSubjects();
+        assertEquals(1, subjects.size());
 
     }
 }
